7ea0fcde9bd0d14bda08c69c6edb535b639c227e
Title_of_the_Page <titleofthepage@gmail.com>
2019-10-21 17:37:39 -0400

    Added global, and local audio, and made a successful build
    Added global, and local audio, and made a successful build

---
+++ UnityLevelWork/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void SceneSwitch()
+    {
+        SceneManager.LoadScene(1); //[int sceneBuildIndex] Tell the scenemanager which scene to load based on a index or variable
+    }
+}
