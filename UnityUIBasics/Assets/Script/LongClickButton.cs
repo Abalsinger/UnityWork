5ea263058389b52ea64a75fec8715c86ef45a91c
Title_of_the_Page <titleofthepage@gmail.com>
2020-03-19 23:53:00 -0400

    Everything back to normal

---
+++ UnityUIBasics/Assets/Script/LongClickButton.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class LongClickButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private bool pointerDown;
+    private float pointerDownTimer;
+
+    public float requiredHoldTime;
+    [SerializeField]
+    private Image fillImage;
+
+    public UnityEvent OnLongClick;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pointerDown = true;
+        Debug.Log("OnPointerDown Error");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Reset();
+        Debug.Log("OnPointerUp Error");
+    }
+
+    public void Update()
+    {
+        if(pointerDown)
+        {
+            pointerDownTimer += Time.deltaTime;
+            if (pointerDownTimer > requiredHoldTime)
+            {
+                if (OnLongClick != null)
+                    OnLongClick.Invoke();
+
+                Reset();
+            }
+            fillImage.fillAmount = pointerDownTimer / requiredHoldTime;
+        }
+    }
+
+    private void Reset()
+    {
+        pointerDown = false;
+        pointerDownTimer = 0;
+        fillImage.fillAmount = pointerDownTimer / requiredHoldTime;
+    }
+}
