3d152af05bdf76a95b8e89aec60ba34bd92fbeac
Title_of_the_Page <titleofthepage@gmail.com>
2020-03-30 22:48:43 -0400

    Profab links

---
+++ MFVprototype/Assets/Scripts/Duo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Duo : MonoBehaviour
+{
+    public void SceneSwitch()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+
+public class Three : MonoBehaviour
+{
+    public void SceneSwitch()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ MFVprototype/Assets/Scripts/Thrace.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Thrace : MonoBehaviour
+{
+    public void SceneSwitch()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
+
